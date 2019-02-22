318030f9657e522e82e5ae11751f315370db2cb1
jiromesarb <jiromesangelbaril@sfernando.sti.edu>
2019-02-22 13:12:25 +0800

    [UPDATE] UPDATED ATTENDANCECONTROLLER

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Auth;
@@ -10,6 +9,7 @@ use App\User;
 use App\Teams;
 use App\Clusters;
 use Faker\Factory as Faker;
+
 
 class AttendanceController extends Controller
 {
@@ -31,31 +31,34 @@ class AttendanceController extends Controller
      }
     public function index()
     {
-        if(Auth::user()->role == base64_encode('tl')){
-            $get_agents = Teams::where('tl_id', Auth::user()->id)->value('agent_code'); // GET TL/CL's AGENT/S
-        }
-        else if(Auth::user()->role == base64_encode('cl')){
-            $get_agents = Clusters::where('cl_id', Auth::user()->id)->value('tl_ids'); // GET TL/CL's AGENT/S
-        }
+        if(Auth::user()->role == base64_encode('cl')){
+            $count_if_multiple = Clusters::where('cl_id', Auth::user()->id)->count();
+            if($count_if_multiple == 1){
 
-        // GET UNPRESENT AGENTS
-        $selected_unpresent_agents = [];
-        foreach($get_agents as $agent){
-            // CHECK IF AGENT IS ALREADY EXISTING ON ATTENDANCE TABLE ON THIS DAY
-            // return $agent;
-            $check_agent = Attendance::where('user_id', $agent)->whereDate('created_at', Carbon::today())->first();
-            if(empty($check_agent) || $check_agent == null){
-                array_push($selected_unpresent_agents, $agent);
             }
         }
-        // return $selected_unpresent_agents;
-        $attendance['unpresent'] = User::whereIn('id', $selected_unpresent_agents)->get();
+        else if(Auth::user()->role == base64_encode('tl')){
+            if($count_if_multiple == 1){
+            $count_if_multiple = Teams::where('tl_id', Auth::user()->id)->count();
+                $identify_attendance = Teams::where('tl_id', Auth::user()->id)->first();
+                $selected_unpresent_agents = [];
+                foreach($identify_attendance['agent_code'] as $agent){
+                    $check_agent = Attendance::where('user_id', $agent)->whereDate('created_at', Carbon::today())->first();
+                    if(empty($check_agent) || $check_agent == null){
+                        array_push($selected_unpresent_agents, $agent);
+                    }
+                }
+            }
+        }
+
+        // GET ROLL CALL
+        return $attendance['unpresent'] = User::whereIn('id', $selected_unpresent_agents)->get();
 
         // GET PRESENT AGENTS
-        $attendance['present'] = Attendance::whereIn('user_id', $get_agents)->whereDate('created_at', Carbon::today())->where('status', 1)->with(['Users'])->get();
+        $attendance['present'] = Attendance::whereIn('user_id', $get_users)->whereDate('created_at', Carbon::today())->where('status', 1)->with(['Users'])->get();
 
         // GET ABSENT AGENTS
-        $attendance['absent'] = Attendance::whereIn('user_id', $get_agents)->whereDate('created_at', Carbon::today())->where('status', 0)->with(['Users'])->get();
+        $attendance['absent'] = Attendance::whereIn('user_id', $get_users)->whereDate('created_at', Carbon::today())->where('status', 0)->with(['Users'])->get();
 
         return view('app.attendance.index', compact('attendance'));
     }
@@ -78,23 +81,27 @@ class AttendanceController extends Controller
      */
     public function store(Request $request)
     {
+        return $team_id = Teams::where('tl_id', Auth::user()->id)->get(['team_id']);
+        return Clusters::where('team_ids', 'like', '%' . $team_id . '%')->get(['cluster_id']);
         // return $request->all();
         $data = [];
         foreach($request->only('user')['user'] as $user){
-            if(!empty($user['selected_row'])){
+            if($user['status'] != null){
                 $set_data = [
+                    "cluster_id" => $user['user_id'],
+                    "team_id" => $user['user_id'],
                     "user_id" => $user['user_id'],
                     "activities" => $user['activities'],
                     "location" => $user['location'],
                     "remarks" => $user['remarks'],
                     "status" => $user['status'],
-                    'created_at' => Carbon::today(),
-                    'updated_at' => Carbon::today(),
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
                 ];
                 array_push($data, $set_data);
             }
         }
-        // return $data;
+        return $data;
         Attendance::insert($data);
         return back();
     }
