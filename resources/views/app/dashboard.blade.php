5b2160621889f2410f704cbbd6167d2cb3dbab11
pollyreal <pollyreal732gmail.com>
2019-03-06 17:31:02 +0800

    Edited found errors and bug division by zero(Done in test server)

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -63,7 +63,7 @@
                                             <p>New: <b>{{ (float)$team->getallsafthiscutoff['new'] }}</b></p>
                                             <p>Activated: <b class="text-info">{{ (float)$team->getallsafthiscutoff['activated'] }}</b></p>
                                             <p>Paid: <b class="text-primary">{{ (float)$team->getallsafthiscutoff['paid'] }}</b></p>
-                                            <p>Target %: <b class="text-success">{{ (float)$team->pat }}%</b></p>
+                                            <p>Target: <b class="text-success">{{ (float)$team->pat }}%</b></p>
                                         </div>
                                     </div>
                                     @endif
