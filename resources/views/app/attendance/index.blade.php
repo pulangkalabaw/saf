2d3229874569d6375973d29353854daf70963b52
jiromesarb <jiromesangelbaril@sfernando.sti.edu>
2019-03-06 16:32:30 +0800

    fixed bugs in attendance index

--- resources/views/app/attendance/index.blade.php
+++ resources/views/app/attendance/index.blade.php
@@ -24,6 +24,28 @@
                             </div>
                             {{-- {{dd(empty($attendance['unpresent']))}} --}}
                             {{-- {{ dd(count(session()->get('_t'))) }} --}}
+                            <div class="panel-body">
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-lg-1 col-md-2 col-sm-2">
+                                            <button onclick="window.location  = '{{ url('app/attendance') . '?date=' . $date['previous'] }}'" class="btn btn-md btn-warning">Previous</button>
+                                        </div>
+                                        <div class="col-lg-3 col-md-4 col-sm-4">
+                                            <div class="input-group date">
+                                                <input class="form-control text-light" name="jump_to_date" type="text" id="jump_to_date" value="{{ $date['selected'] }}" max="2019-03-04" readonly>
+                                                <div class="input-group-addon" title="Jump to certain date" id="icon-container">
+                                                    <div class="fa fa-calendar" title="Jump to certain date"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-1 col-md-2 col-sm-2">
+                                            <button onclick="window.location  = '{{ url('app/attendance') . '?date=' . $date['next'] }}'" {{ $date['next'] == null ? 'disabled' : '' }} class="btn btn-md btn-warning">Next</button>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                    </div>
+                                </div>
+                            </div>
                             @if(count(session()->get('_c')) == 0 && count(session()->get('_t')) == 0)
                                 <div class="panel-body">
                                     <div class="form-group text-center">
